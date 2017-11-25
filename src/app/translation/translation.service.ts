485c69b7366fe69bc5e8acb4a8a44cf58ff1228a
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2017-11-25 03:14:36 +0100

    Reorder imports.

--- src/app/translation/translation.service.ts
+++ src/app/translation/translation.service.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 
 import { GoogleCloudApiKey, insertAt } from '../../libs';
